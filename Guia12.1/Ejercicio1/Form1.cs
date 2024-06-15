7b5507cfd6f0baad6e821226313febf7579f3fe0
facundonadalin <facundoenadalin@gmail.com>
2024-06-14 21:13:08 -0300

    instanciacion de la clase controlador

--- Guia12.1/Ejercicio1/Form1.cs
+++ Guia12.1/Ejercicio1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ejercicio1.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,16 @@ namespace Ejercicio1
 {
     public partial class Form1 : Form
     {
+        Controlador ejemplo= new Controlador();
         public Form1()
         {
+
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
